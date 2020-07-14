7b08e58cbe2b959146b68df1578d1fe4a5f0dcb2
Alex <alex.banar10@gmail.com>
2020-07-14 20:26:19 +0300

    Updates

---
+++ precache-manifest.e58d6f78b1e137975b9d304a1a72f2cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c3f844ae4538184b2a1c46e1a592d0",
+    "url": "/Tic-Tac-Toe-react-app/index.html"
+  },
+  {
+    "revision": "9a4a38dc8ebf06e99746",
+    "url": "/Tic-Tac-Toe-react-app/static/css/main.929c21f2.chunk.css"
+  },
+  {
+    "revision": "7ab2e661415f24027f78",
+    "url": "/Tic-Tac-Toe-react-app/static/js/2.1f25e168.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe-react-app/static/js/2.1f25e168.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a4a38dc8ebf06e99746",
+    "url": "/Tic-Tac-Toe-react-app/static/js/main.b9880053.chunk.js"
+  },
+  {
+    "revision": "db0053b04b92cc572efd",
+    "url": "/Tic-Tac-Toe-react-app/static/js/runtime-main.82fba895.js"
+  }
+]);
